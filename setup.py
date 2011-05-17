57fef79bcad6ac5af94f8b767f5c7f1959cec92c
Paul Lathrop <paul@simplegeo.com>
2011-05-16 17:25:35 -0700

    Add missing package.

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from glob import glob
 setup(name='metartg',
       version='0.1',
       author='Jeremy Grosser',
-      packages=['metartg', 'metartg.checks', 'dewpoint', 'metartg.thrift'],
+      packages=['metartg', 'metartg.checks', 'dewpoint', 'metartg.thrift',
+                'metartg.thrift.cassandra'],
       scripts=[
         'scripts/metartg_check',
         'scripts/metartg_consumer'
