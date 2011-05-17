e99b930a0b14020246374bd59d19238e56208b89
Paul Lathrop <paul@simplegeo.com>
2011-05-16 17:50:33 -0700

    Add cassandra column family cache metrics.

--- metartg/checks/cassandra.py
+++ metartg/checks/cassandra.py
@@ -3,6 +3,75 @@ import simplejson as json
 from time import time
 import subprocess
 import os
+import urllib2
+
+from thrift import Thrift
+from thrift.transport import TTransport
+from thrift.transport import TSocket
+from thrift.protocol.TBinaryProtocol import TBinaryProtocolAccelerated
+from metartg.thrift.cassandra import Cassandra
+from metartg.thrift.cassandra.ttypes import *
+
+def get_local_ipv4():
+    # This only works on ec2 and should thus be made better. But it
+    # was the quickest way I could think of to gett the IP (no,
+    # 'localhost' won't work.)
+    url = 'http://169.254.169.254/latest/meta-data/local-ipv4'
+    return urllib2.urlopen(url).read()
+
+def get_keyspaces():
+    socket = TSocket.TSocket(get_local_ipv4(), 9160)
+    transport = TTransport.TFramedTransport(socket)
+    protocol = TBinaryProtocolAccelerated(transport)
+    client = Cassandra.Client(protocol)
+    try:
+        transport.open()
+        keyspaces = client.describe_keyspaces()
+    except Thrift.TException, tx:
+        print 'Thrift: %s' % tx.message
+    finally:
+        transport.close()
+
+    return keyspaces
+
+def get_column_families(keyspace):
+    socket = TSocket.TSocket(get_local_ipv4(), 9160)
+    transport = TTransport.TFramedTransport(socket)
+    protocol = TBinaryProtocolAccelerated(transport)
+    client = Cassandra.Client(protocol)
+    try:
+        transport.open()
+        column_families = client.describe_keyspace(keyspace)
+    except Thrift.TException, tx:
+        print 'Thrift: %s' % tx.message
+    finally:
+        transport.close()
+
+    return column_families
+
+def cfstats_cache_metrics():
+    keyspaces = get_keyspaces()
+    now = int(time())
+
+    metrics = {}
+    for keyspace in keyspaces:
+        column_families = get_column_families(keyspace)
+        for column_family in column_families.keys():
+            for cache_type in ('Key', 'Row'):
+                url = 'http://%s:8778/jolokia/read/' \
+                    'org.apache.cassandra.db:cache=' \
+                    '%s%sCache,keyspace=%s,type=Caches' % (
+                    get_local_ipv4(), column_family, cache_type, keyspace)
+                cache_stats = json.load(urllib2.urlopen(url))
+                for label in ('RecentHitRate', 'Capacity', 'Size'):
+                    metrics['%s-%s_%sCache%s' % (keyspace, column_family, cache_type, label)] = {
+                        'ts': now,
+                        'type': 'GAUGE',
+                        'value': cache_stats['value'][label],
+                        }
+
+    return metrics
+
 
 def tpstats_metrics():
     p = subprocess.Popen([
@@ -39,7 +108,7 @@ def sstables_metrics():
     for dirname in ('penelope', 'cassandra'):
         if os.path.exists('/mnt/var/lib/' + dirname):
             break
-        
+
     for keyspace in os.listdir('/mnt/var/lib/%s/data' % dirname):
         now = int(time())
         sizes = {}
