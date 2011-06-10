0404790cb3cf05b48a3b30dc097a19aaa0c4a038
Jeremy Grosser <jeremy@synack.me>
2011-06-10 15:20:38 -0700

    Add new graph definition format

--- metartg/server.py
+++ metartg/server.py
@@ -610,6 +610,102 @@ def search():
 
     return dumps(servers)
 
+GRAPHDEFS = '/home/synack/src/metartg/graphs'
+RRDS = '/var/lib/metartg/rrds/'
+
+@bottle.get('/graph/:host/:service/:graph')
+def get_graph(host, service, graph):
+    graphdefs = json.load(file('%s/%s.json' % (GRAPHDEFS, service), 'r'))
+    if not graph in graphdefs:
+        bottle.abort(404, 'No graph definition named %s in %s.json' % (graph, service))
+    graphdef = graphdefs[graph]
+
+    now = int(time())
+    params = bottle.request.params
+    start = params.get('start', (now - 3600))
+    end = params.get('end', now)
+    size = params.get('size', 'large')
+
+    cmd = ['/usr/bin/rrdtool', 'graph',
+        '-',
+        '--daemon', '127.0.0.1:42217',
+        '--font', 'DEFAULT:7:monospace',
+        '--font-render-mode', 'normal',
+        '--color', 'MGRID#880000',
+        '--color', 'GRID#777777',
+        '--color', 'CANVAS#000000',
+        '--color', 'FONT#ffffff',
+        '--color', 'BACK#444444',
+        '--color', 'SHADEA#000000',
+        '--color', 'SHADEB#000000',
+        '--color', 'FRAME#444444',
+        '--color', 'ARROW#FFFFFF',
+        '--imgformat', 'PNG',
+        '--tabwidth', '75',
+        '--start', str(start),
+        '--end', str(end),
+    ]
+
+    if 'rrdargs' in graphdef:
+        cmd += graphdef['rrdargs'].split(' ')
+
+    if 'title' in graphdef:
+        cmd += ['--title', '%s | %s' % (get_clusto_name(host), graphdef['title'])]
+
+    if size == 'small':
+        cmd += [
+            #'--no-legend',
+            '--width', '375',
+            '--height', '100'
+        ]
+    else:
+        cmd += [
+            '--width', '600',
+            '--height', '200',
+            '--watermark', 'simplegeo'
+        ]
+
+    for defname, rrdpath in graphdef['rrds'].items():
+        rrdpath = os.path.join(RRDS, host, rrdpath)
+        cmd.append('DEF:%s=%s:sum:AVERAGE' % (defname, rrdpath))
+    cmd += graphdef['config']
+
+    print '\n'.join(cmd)
+
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, env={'TZ': 'PST8PDT'})
+    stdout, stderr = proc.communicate()
+
+    bottle.response.content_type = 'image/png'
+    return stdout
+
+@bottle.get('/graph/:host')
+def get_available_graphs(host):
+    available = []
+    rrdpath = os.path.join(RRDS, host)
+    if not rrdpath.startswith(RRDS):
+        bottle.abort(400, 'That\'s not right...')
+
+    for filename in os.listdir(GRAPHDEFS):
+        if not filename.endswith('.json'):
+            continue
+        path = os.path.join(GRAPHDEFS, filename)
+        try:
+            graphs = json.load(file(path, 'r'))
+        except ValueError, e:
+            bottle.abort(500, 'Error parsing graph definition file %s: %s' % (filename, str(e)))
+
+        service = filename.rsplit('.', 1)[0]
+        for name, graph in graphs.iteritems():
+            if not all([os.path.exists(os.path.join(rrdpath, rrdfile)) for rrdfile in graph['rrds'].values()]):
+                continue
+            available.append('%s/%s' % (service, name))
+    return dumps(available)
+
+@bottle.get('/graph/')
+def get_available_hosts():
+    return dumps(os.listdir(RRDS))
+
+
 @bottle.get('/static/:filename')
 def serve_static(filename):
     return bottle.static_file(filename, root=STATIC_PATH)
