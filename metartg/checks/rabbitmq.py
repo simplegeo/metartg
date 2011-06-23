87f809c62f6f9222d374d2c19bb0c6e32596d132
mihasya <m@mihasya.com>
2011-06-23 14:19:39 -0700

    add rabbitmq rate metrics

--- metartg/checks/rabbitmq.py
+++ metartg/checks/rabbitmq.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from time import time
 import subprocess
+import json
+
 
 def rabbitmq_metrics():
     p = subprocess.Popen(['/usr/sbin/rabbitmqctl', '-q', 'list_queues', '-p', 'simplegeo', 'name', 'messages', 'messages_unacknowledged'], stdout=subprocess.PIPE)
@@ -22,6 +24,41 @@ def rabbitmq_metrics():
         }
     return metrics
 
-    
+
+def rabbitmq_rate_metrics():
+    p = subprocess.Popen(['/usr/bin/curl', '-s', '-u', 'simplegeo:simplegeo', 'http://localhost:55672/api/queues/simplegeo'], stdout=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    try:
+        queues = json.loads(stdout)
+    except ValueError:
+        return None
+
+    now = int(time())
+    metrics = {}
+    for queue in queues:
+        # rate at which msgs are being acknowledged, I think...
+        metrics['%s_ack_rate' % queue['name']] = {
+            'ts': now,
+            'type': 'GAUGE',
+            'value': long(queue['backing_queue_status']['avg_ack_ingress_rate']),
+        }
+        # rate at which messages are coming in
+        metrics['%s_incoming_rate' % queue['name']] = {
+            'ts': now,
+            'type': 'GAUGE',
+            'value': long(queue['backing_queue_status']['avg_ingress_rate']),
+        }
+    return metrics
+
+
 def run_check(callback):
     callback('rabbitmq', rabbitmq_metrics())
+    rates_result = rabbitmq_rate_metrics()
+    if rates_result:
+        callback('rabbitmq', rates_result)
+
+
+if __name__ == '__main__':
+    print json.dumps(rabbitmq_metrics(), indent=2)
+    print json.dumps(rabbitmq_rate_metrics(), indent=2)
+
