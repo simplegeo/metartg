1b79c38bb1aa73fd6287b28dc20594554fe803c9
dsmith <derek@dsmitts.com>
2011-07-01 10:13:42 -0700

    Added a new check for disk usage

--- metartg/checks/disk.py
+++ metartg/checks/disk.py
@@ -3,6 +3,7 @@ from collections import namedtuple
 from time import time
 import subprocess
 import simplejson as json
+import os
 
 
 IOStat = namedtuple('IOStat', 'rrqm wrqm reads writes rkb wkb avgrq avgq await svctm util')
@@ -27,5 +28,38 @@ def disk_metrics():
     return metrics
 
 
+def disk_space_metrics():
+    if not os.path.exists('/proc/mounts'):
+        return {}
+
+    metrics = {}
+    for l in file('/proc/mounts'):
+        if l[0] == '/':
+            l = l.split()
+            device = l[0]
+            path = l[1]
+            stats = os.statvfs(path)
+            device_metrics = {
+                    'path': path,
+                    'preferred_block_size': int(stats.f_bsize),
+                    'fundamental_block_size': int(stats.f_blocks),
+                    'total_blocks': int(stats.f_blocks),
+                    'free_blocks' : int(stats.f_bfree),
+                    'available_blocks' : int(stats.f_bavail),
+                    'total_file_nodes' : int(stats.f_files),
+                    'free_file_nodes' : int(stats.f_ffree)
+                    }
+
+            now = int(time())
+            for name, value in device_metrics.items():
+                metrics['%s.%s' % (device, name)] = {
+                        'value': value,
+                        'ts': now,
+                        'type': 'GAUGE'
+                        }
+    return metrics
+
+
 def run_check(callback):
     callback('disk', disk_metrics())
+    callback('disk-space', disk_space_metrics())
