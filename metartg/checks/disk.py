d2eaae27ed5ea1b5ae7ff9f295678f1b04c5ee99
dsmith <derek@dsmitts.com>
2011-07-01 11:04:38 -0700

    Only graph the metrics that we want

--- metartg/checks/disk.py
+++ metartg/checks/disk.py
@@ -41,13 +41,9 @@ def disk_space_metrics():
             path = l[1]
             stats = os.statvfs(path)
             device_metrics = {
-                    'preferred_block_size': int(stats.f_bsize),
-                    'fundamental_block_size': int(stats.f_blocks),
-                    'total_blocks': int(stats.f_blocks),
-                    'free_blocks' : int(stats.f_bfree),
-                    'available_blocks' : int(stats.f_bavail),
-                    'total_file_nodes' : int(stats.f_files),
-                    'free_file_nodes' : int(stats.f_ffree)
+                    'free_space' : int(stats.f_bfree * stats.f_frsize),
+                    'used_nodes' : int(stats.f_files),
+                    'free_nodes' : int(stats.f_ffree)
                     }
 
             now = int(time())
