2b8ffecb5ed13e4eb40efe79de3a52e9e31322c8
mihasya <m@mihasya.com>
2011-08-04 10:15:14 -0700

    make the incoming line dashed so that we can see when the lines overlap

--- metartg/server.py
+++ metartg/server.py
@@ -427,8 +427,8 @@ for queue in queues_list:
     RRD_GRAPH_DEFS[rabbitmq_rate_graph] = [
         'DEF:in=%%(rrdpath)s/rabbitmq/%s_incoming_rate.rrd:sum:AVERAGE' % queue,
         'DEF:ack=%%(rrdpath)s/rabbitmq/%s_ack_rate.rrd:sum:AVERAGE' % queue,
-        'LINE:in#FF3484:%s incoming/s\\l' % queue,
         'LINE:ack#4EFF4D:%s acknowledged/s\\l' % queue,
+        'LINE:in#FF3484:%s incoming/s\\l:dashes=10,5' % queue,
     ]
     RRD_GRAPH_TITLE[rabbitmq_rate_graph] = '%%(host)s | %s queue rates' % queue
     RRD_GRAPH_TYPES.append((rabbitmq_rate_graph, '%s rates' % queue))
